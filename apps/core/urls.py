0fc001491039b0297ce158166d5a0e9870ab94f4
hamidusman <abdulhamidusman218@gmail.com>
2024-10-18 10:39:14 +0100

    product url and endpoints

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from . import views
+
+router = DefaultRouter()
+router.register(r'products', views.ProductViewSet, basename='product')
 
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from .models import Product, ProductImage, Rating
+from .serializers import (ProductSerializer,
+                        ProductImageSerializer,
+                        RatingSerializer,
+                        )
+from rest_framework.response import Response
+from rest_framework import viewsets, generics
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+    
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response("Product Deleted Successfully")
